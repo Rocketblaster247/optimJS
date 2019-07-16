4c9fefb46bad7f2186c643cb3ef4b8a0f794f1bf
RocketBlaster <47755918+Rocketblaster247@users.noreply.github.com>
2019-07-16 16:20:31 -0400

    Update optimjs.js

--- optimjs.js
+++ optimjs.js
@@ -16,22 +16,16 @@ var runProgram = function (array) {
 //  t = times the number should increment,
 //  fpi = frames per iteration
 //);
-var loop = function (f, n, t, fpi) {
+var loop = function (f, a, b) {
   var jbnNow = Date.now();
-  var e = f.toString().split("function () {")[1];
-  console.log(e);
-  e = e.toString().split("}")[e.toString().split("}").length-2];
-  console.log(e);
-  e = 'for (var ' + escape(n) + ' = 0; ' + escape(n) +' < ' + fpi + '; ' + escape(n) + '++){' + e + '}';
   var index = 0;
   var _a = function () {
-    if (index < (t*fpi) + fpi) {
-      eval(e);
-      window.requestAnimationFrame(_a);
-      index ++;
-    } else {
-      
+    for (var i = 0; i < b || 30; i ++) {
+      if (index < a) {
+        f((index*b)+i);
+        index ++;
+        window.requestAnimationFrame(_a);
+      }
     }
   };
-  _a();
 };

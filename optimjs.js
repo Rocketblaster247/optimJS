b68e6deedb02d1fb229ee62d5f367ffbbd7c0e38
RocketBlaster <47755918+Rocketblaster247@users.noreply.github.com>
2019-07-16 17:02:36 -0400

    Update optimjs.js

--- optimjs.js
+++ optimjs.js
@@ -19,17 +19,21 @@ var runProgram = function (array) {
 var loop = function (f, a, b) {
   var jbnNow = Date.now();
   var index = 0;
+  var done = false;
   var _a = function () {
     for (var i = 0; i < (b || 30); i ++) {
       if (index < (a*b)) {
         f((index*b)+i);
         index ++;
         window.requestAnimationFrame(_a);
+      } else {
+        var s = Date.now() - jbnNow;
+        var fps = Math.floor((a*b)/(s/1000));
+        console.log("OptimJS: Loaded function in " + s + "ms (" + fps + "fps)");
+        done = true;
       }
     }
   };
   _a();
-  var s = Date.now() - jbnNow;
-  var fps = Math.floor((a*b)/(s/1000));
-  console.log("OptimJS: Loaded function in " + s + "ms (" + fps + "fps)");
+  
 };

1cb9ab9c89454563522627ca33198abb010a9808
RocketBlaster <47755918+Rocketblaster247@users.noreply.github.com>
2019-07-16 13:53:48 -0400

    Create optimjs.js

---
+++ optimjs.js
@@ -0,0 +1,21 @@
+var runProgram = function (array) {
+  var index = 0;
+  var _a = function () {
+    if (index < array.length - 1) {
+      array[index]();
+      index ++;
+      window.requestAnimationFrame();
+    }
+  };
+  _a();
+};
+var loop = function (f, i) {
+  var e = i;
+  var _a = function () {
+    if (i != 0) {
+      f(e - i);
+      i --;
+      window.requestAnimationFrame();
+    }
+  };
+};
